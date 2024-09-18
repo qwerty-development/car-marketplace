4065c620f3ad3a2f732ce8a230fdcdddd3f35023
Assif Alam <asif.k.alam@net.usek.edu.lb>
2024-09-18 13:33:37 +0300

    gradient fix

--- app/(home)/(user)/(tabs)/index.tsx
+++ app/(home)/(user)/(tabs)/index.tsx
@@ -332,12 +332,20 @@ export default function BrowseCarsPage() {
 
 	return (
 		<LinearGradient
-			colors={isDarkMode ? ['#000000', '#D55004'] : ['#FFFFFF', '#D55004']}
+			colors={
+				isDarkMode
+					? ['#000000', '#1A1A1A', '#333333', '#666666', '#A63E07'] // Stronger black, subtler orange in dark mode
+					: ['#FFFFFF', '#F2F2F2', '#E6E6E6', '#CCCCCC', '#FFA07A'] // Stronger white, softer orange in light mode
+			}
 			style={{ flex: 1 }}
-			start={{ x: 1, y: 0.3 }}
-			end={{ x: 2, y: 1 }}>
+			start={{ x: 0, y: 0 }}
+			end={{ x: 1, y: 1 }}>
 			<SafeAreaView
-				style={[styles.container, isDarkMode && styles.darkContainer]}>
+				style={[
+					styles.container,
+					isDarkMode && styles.darkContainer,
+					{ backgroundColor: 'transparent' }
+				]}>
 				<View style={styles.searchContainer}>
 					<View style={styles.searchInputContainer}>
 						<TouchableOpacity
